72f61f1a575ded9705490e50a196ec92e0e4d926
Patrick-Nduka <patorisky1@gmail.com>
2024-08-22 13:41:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My APP with GITHUB
+          My APP with 
         </p>
         <a
           className="App-link"
